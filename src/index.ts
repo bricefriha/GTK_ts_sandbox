4c51e4f84bed387588e66fee1d52d56a142207f6
bricefriha <brice.friha@outlook.com>
2022-02-17 09:07:33 +0000

    First commit

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+//@ts-ignore
+import gi from "node-gtk";
+const Gtk = gi.require("Gtk", "3.0");
+
+gi.startLoop();
+Gtk.init();
+
+const win = new Gtk.Window();
+win.on("destroy", () => Gtk.mainQuit());
+win.on("delete-event", () => false);
+
+win.setDefaultSize(200, 80);
+win.add(new Gtk.Label({ label: "Hello Gtk+" }));
+
+win.showAll();
+Gtk.main();
